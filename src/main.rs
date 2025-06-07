180ddc84f612a86ce47caa96011ae14492aa4701
lbrealdev <lbrealdeveloper@gmail.com>
2025-06-07 23:55:18 +0200

    feat: create fetch_api function and improvement handle errors

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use anyhow::{Result, anyhow};
 use colored::*;
 use reqwest::Client;
 use serde::{Deserialize, Serialize};
+use std::result::Result::Ok;
 use std::{env, process, time::Duration};
 
 #[derive(Debug, Deserialize, Serialize)]
@@ -28,31 +29,22 @@ async fn main() -> Result<()> {
         .timeout(Duration::from_secs(5))
         .build()?;
 
-    let response = client
-        .post(API_URL)
-        .header("apikey", token)
-        .body(body)
-        .send()
-        .await;
+    match fetch_api(&client, &token, body).await {
+        Ok(msg) => {
+            let json = serde_json::json!({
+                "input_content": msg.content,
+                "output_content": msg.censored_content,
+                "bad_words_total": msg.bad_words_total,
+            });
 
-    match response {
-        Ok(res) => match res.json::<Message>().await {
-            Ok(msg) => {
-                let json = serde_json::json!({
-                    "input_content": msg.content,
-                    "output_content": msg.censored_content,
-                    "bad_words_total": msg.bad_words_total,
-                });
+            let json_pretty = serde_json::to_string_pretty(&json)?;
 
-                let json_pretty = serde_json::to_string_pretty(&json)?;
-
-                println!("{}", json_pretty.bright_green());
-            }
-            Err(e) => {
-                eprintln!("JSON convert error: {e}");
-            }
-        },
-        Err(e) if e.is_timeout() => {
+            println!("{}", json_pretty.bright_green());
+        }
+        Err(e)
+            if e.downcast_ref::<reqwest::Error>()
+                .is_some_and(|e| e.is_timeout()) =>
+        {
             eprintln!("Timeout! The request took too long to complete.");
             process::exit(1);
         }
@@ -63,4 +55,17 @@ async fn main() -> Result<()> {
     }
 
     Ok(())
+}
+
+async fn fetch_api(client: &Client, token: &str, body: String) -> Result<Message> {
+    let response = client
+        .post(API_URL)
+        .header("apikey", token)
+        .body(body)
+        .send()
+        .await?;
+
+    let msg = response.json::<Message>().await?;
+
+    Ok(msg)
 }
